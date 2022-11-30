734add9961076f41b834fe50c624674fa8edfaca
main-voice <voicemainid73@gmail.com>
2022-12-01 01:52:36 +0800

    permission based auth with preAuthorize

--- src/main/java/com/example/demo/student/StudentManagementController.java
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -24,23 +25,27 @@ public class StudentManagementController {
     );
 
     @GetMapping(path = "all")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public static List<Student> getSTUDENTS() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerStudent(@RequestBody Student student) {
         System.out.println("register : ");
         System.out.println(student.toString());
     }
 
     @DeleteMapping(path = "{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("id") Integer id) {
         System.out.println("delete : ");
         System.out.println(id);
     }
 
     @PutMapping(path = "{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("id") Integer id,
                               @RequestBody Student student) {
         System.out.println("update : ");
